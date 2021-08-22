8c6bffba92b2e6d0127ea698620287229b9159cf
dbledime1 <dbledime@yahoo.com>
2021-08-22 13:46:04 -0500

    initial

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgresdb
+export POSTGRES_PASSWORD=denier93
+export POSTGRES_HOST=postgres.c4asaqqbaqtj.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::p3-adm
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=goodby
+
+export URL=http://localhost:8100
