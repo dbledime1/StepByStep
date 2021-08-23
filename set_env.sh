168b5f2cb32e49892f1cfa8b419520b0aab35ec6
dbledime1 <dbledime@yahoo.com>
2021-08-22 22:37:08 -0500

    final commit

--- set_env.sh
+++ set_env.sh
@@ -2,11 +2,11 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgresdb
 export POSTGRES_PASSWORD=denier93
-export POSTGRES_HOST=postgres.c4asaqqbaqtj.us-east-2.rds.amazonaws.com
+export POSTGRES_HOST=postgres.cdcemqdytlgr.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::p3-adm
 export AWS_REGION=us-east-2
 export AWS_PROFILE=default
-export JWT_SECRET=hellow
+export JWT_SECRET=hello
 
 export URL=http://localhost:8100
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://abe237a1ef61e4a77895e04b770bbc36-1248364891.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
