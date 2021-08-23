e3335411ad9f56ceaacec1ac9507387aee44c6b9
dbledime1 <87278896+dbledime1@users.noreply.github.com>
2021-08-22 22:39:58 -0500

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,12 +1,12 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgresdb
-export POSTGRES_PASSWORD=denier93
-export POSTGRES_HOST=postgres.cdcemqdytlgr.us-east-2.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::p3-adm
-export AWS_REGION=us-east-2
-export AWS_PROFILE=default
-export JWT_SECRET=hello
+export POSTGRES_USERNAME=
+export POSTGRES_PASSWORD=
+export POSTGRES_HOST=
+export POSTGRES_DB=
+export AWS_BUCKET=a
+export AWS_REGION=u
+export AWS_PROFILE=d
+export JWT_SECRET=h
 
 export URL=http://localhost:8100
